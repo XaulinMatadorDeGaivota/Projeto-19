8a4a42aa42f155ad50d2f32e80e72f912cb2412a
Matheus Cabral Calil <93673317+XaulinMatadorDeGaivota@users.noreply.github.com>
2021-12-07 12:41:29 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var fox,foxImg;
+var fox,foxImg,foxGroup;
 var rat,ratImg;
 
 var background,backgroundImg;
@@ -17,47 +17,60 @@ function preload(){
 function setup() {
   createCanvas(600,300); 
 
+  invisibleGround = createSprite(100,355,100,100);
+  invisibleGround.visible = true;
+
   background = createSprite(300,100);
   background.addImage("background",backgroundImg);
 
-  fox = createSprite(450,270,20,20);
+  fox = createSprite(450,275,20,20);
   fox.addImage("fox",foxImg);
-  fox.scale = 0.2;
+  fox.scale = 0.15;
 
-  rat = createSprite(100,275,10,10);
+  rat = createSprite(100,280,10,10);
   rat.addImage("rat",ratImg);
-  rat.scale = 0.5;
+  rat.scale = 0.4;
 }
 function draw() {
 
  if(keyDown("space")){
-    rat.y = rat.y-7;
+    rat.y = rat.y-10;
     rat.velocityY = rat.velocityY+0.2;
  }
  
-  if(gameState === "play"){
-    background.velocityX = -3;
-  }
+ if(gameState === "play"){
+  background.velocityX = -3;
+  fox.velocityX =-3;
+}
 
   if(rat.isTouching(fox)){
     rat.destroy();
-    gameState === "end"
+    gameState == "end"
   }
 
   if(background.x < 0){
     background.x = 600;
   }
 
+  rat.collide(invisibleGround);
+
+  fox.setCollider["rectangle",0,0,fox,width,fox,height];
+  fox.debug = true
+
+  rat.setCollider["rectangle",0,0,rat,width,rat,height];
+  rat.debug = true
+
+
  invisibleBlock();
 
  drawSprites();
 
  if(gameState === "end"){
-  stroke("yellow");
-  fill("yellow");
-  textSize(30);
-  text("Game Over!",300,150);
- }
+  text("Game Over!",250,250);
+  fox.velocityX = 0;
+  rat.velocityX = 0;
+  background.velocityX = 0;
+}
 }
 
 function invisibleBlock(){
